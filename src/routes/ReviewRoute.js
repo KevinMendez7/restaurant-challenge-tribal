81e37b4b2bcc52dfc33dbddb364bb3055da42683
= <=>
2021-07-20 21:07:13 -0600

    review route
    get by id, get all, get reviews by restaurant, post review for specific restaurant created

---
+++ src/routes/ReviewRoute.js
@@ -0,0 +1,108 @@
+const mongoose = require('mongoose');
+
+const Review = mongoose.model('Review');
+const Restaurant = mongoose.model('Restaurant');
+
+module.exports = app => {    
+
+  app.get('/review/:id', async (req, res) => {
+    try {
+      const review = await Review.findById(req.params.id).populate('restaurant');
+      res.json(review);
+      
+    } catch(err) {
+      console.error(err);
+      next('Something happen');
+    }
+  });
+
+  app.get('/review', async (req, res) => {
+    try {
+      const reviews = await Review.find({});      
+      res.json(reviews);
+      
+    } catch(err) {
+      console.error(err);
+      next('Something happen');
+    }
+  });
+
+    app.get('/restaurant/:id/review', async ({ params : { id : _id } }, res, next) => {
+      try {
+
+        if(!mongoose.isValidObjectId(_id)) {
+          return res.json({
+            error: 'param id is not valid, must be a single String of 12 bytes or a string of 24 hex characters'
+          });
+        }
+
+        if(!_id) {
+          return res.json({
+            error: 'Restaurant param id is required for looking reviews'
+          });
+        }
+
+        const restaurant = await Restaurant.findOne({ _id }).populate('reviews');
+
+        if(!restaurant) {
+          return res.json({
+            error: 'Restaurant with given id not found!'
+          });
+        }
+
+        const { reviews } = restaurant;
+        
+        let msg = 'OK';
+
+        if(!reviews.length > 0) {
+          msg = `No reviews found for given restaurant(${_id})`
+        }
+        
+        res.json({ data :reviews, msg });                      
+
+      } catch(err) {
+        console.error(err);
+        next('Something failed');        
+      }        
+    });
+
+    app.post('/restaurant/:id/review', async ({ body, params : { id: _id } }, res) => {
+      try {
+        const { name, comment } = body;
+        
+
+        if(!mongoose.isValidObjectId(_id)) {
+          return res.json({
+            error: 'param id is not valid, must be a single String of 12 bytes or a string of 24 hex characters'
+          });
+        }
+
+        if(!_id) {
+          return res.json({
+            error: 'Restaurant param id is required for looking reviews'
+          });
+        }
+
+        const restaurant = await Restaurant.findOne({ _id });
+        
+        if(!restaurant) {
+          return res.json({
+            error: 'Restaurant with given id not found!'
+          });
+        }
+        const review = new Review({ name, comment, restaurant : restaurant._id });
+
+        await review.save();
+
+        restaurant.reviews.push(review._id);
+
+        await restaurant.save()
+
+        res.json(review);            
+  
+      } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+      }
+    });
+};
